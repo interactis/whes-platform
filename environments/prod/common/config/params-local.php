553b153cffb7db9c966705fbcbdd1728172a6974
interactis <dominik@interactis.ch>
2021-04-26 11:31:08 +0200

    Environments

--- environments/prod/common/config/params-local.php
+++ environments/prod/common/config/params-local.php
@@ -1,3 +1,8 @@
 <?php
 return [
+	'frontendUrl' => 'https://ourheritage.ch/',
+	'backendUrl' => 'https://admin.ourheritage.ch/',
+	
+    'trailPoiBuffer' => 200, // 200 m
+    'mapWmsUrl' => 'http://map.jungfraualetsch.ch/geoserver-unesco_aletsch-main/',
 ];

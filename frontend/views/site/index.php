8b36f1de0b32d4dad90f91792e2bb949f33614e6
interactis <dominik@interactis.ch>
2021-05-04 18:41:47 +0200

    Style setup

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -6,48 +6,41 @@ $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
+	<div id="top-carousel" class="jumbotron carousel slide parallax no-title" data-ride="carousel" data-touch="true">
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+	<div class="carousel-inner">
+	
+					<div class="carousel-item active">
+				<img class="hidden" src="//images.ctfassets.net/j6s9c8k7qcjg/2C5JesuhBReeYPZsuOo0VO/684b4dddc149d0ea43e2879841a32cff/20190214-DSC05414.jpg?w=1600&fm=jpg"> 				<div class="img-bg" style="background-image: url('//images.ctfassets.net/j6s9c8k7qcjg/2C5JesuhBReeYPZsuOo0VO/684b4dddc149d0ea43e2879841a32cff/20190214-DSC05414.jpg?w=1600&fm=jpg');"></div>
+			</div>
+					<div class="carousel-item ">
+				<img class="hidden" src="//images.ctfassets.net/j6s9c8k7qcjg/UVKHLwqwxbMP431fYRLZo/17ff3137a9ea1fb3d60804156620d854/GVWAG_AMMANN_20190912-75.jpg?w=1600&fm=jpg"> 				<div class="img-bg" style="background-image: url('//images.ctfassets.net/j6s9c8k7qcjg/UVKHLwqwxbMP431fYRLZo/17ff3137a9ea1fb3d60804156620d854/GVWAG_AMMANN_20190912-75.jpg?w=1600&fm=jpg');"></div>
+			</div>
+					<div class="carousel-item ">
+				<img class="hidden" src="//images.ctfassets.net/j6s9c8k7qcjg/1YxcJfFqC2VaV7M6u3aCVR/1006a595787e3b13d0f571e2b6cad78f/WWFHangar_AMMANN_20190118-14.jpg?w=1600&fm=jpg"> 				<div class="img-bg" style="background-image: url('//images.ctfassets.net/j6s9c8k7qcjg/1YxcJfFqC2VaV7M6u3aCVR/1006a595787e3b13d0f571e2b6cad78f/WWFHangar_AMMANN_20190118-14.jpg?w=1600&fm=jpg');"></div>
+			</div>
+					<div class="carousel-item ">
+				<img class="hidden" src="//images.ctfassets.net/j6s9c8k7qcjg/1WPUt9mNIt2cxnkiLSik3T/6d9962643af9d5accf2055e587c1a681/ILIANO_1801-06232.jpg?w=1600&fm=jpg"> 				<div class="img-bg" style="background-image: url('//images.ctfassets.net/j6s9c8k7qcjg/1WPUt9mNIt2cxnkiLSik3T/6d9962643af9d5accf2055e587c1a681/ILIANO_1801-06232.jpg?w=1600&fm=jpg');"></div>
+			</div>
+				
+					<a class="carousel-control-prev img-link" href="#top-carousel" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next img-link" href="#top-carousel" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+				
+	</div>
+	
+	<div class="footer text-center diagonal">
+		<div class="footer-content">
+					</div>
+		<div class="transition"></div>
+		<div class="spacer small"></div>
+	</div>
+</div>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
-    </div>
+    
 </div>

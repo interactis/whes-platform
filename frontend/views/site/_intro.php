74d388eb9c596bd53a8a4c0f3b66c98f5fe25186
interactis <dominik@interactis.ch>
2021-05-14 17:42:51 +0200

    Intro and overview map

--- frontend/views/site/_intro.php
+++ frontend/views/site/_intro.php
@@ -1,22 +1,23 @@
-<div class="section" id="intro">
-	<div class="section-wrapper">  
-         <header class="container">
-            <div class="row">
-               <div class="col-md-10 col-lg-7">
-                  <h1 class="margin-bottom-md">
-                     UNESCO Welterbe Entdecken
-                  </h2>
-               </div>
-            </div>
-         </header>
-         <div class="container margin-bottom-lg">
-            <div class="row">
-               <div class="col-md-6 col-lg-5">
-               		<div class="margin-bottom">
-                  		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-               		</div>
-               </div>
-            </div>
-         </div>
-	</div>
+<div id="intro">
+	 <header class="container">
+		<div class="row">
+		   <div class="col-md-10 col-lg-7">
+			  <h1 class="margin-bottom-md">
+				 UNESCO Welterbe entdecken
+			  </h2>
+		   </div>
+		</div>
+	 </header>
+	 <div class="container margin-bottom-lg">
+		<div class="row">
+		   <div class="col-md-6 col-lg-5">
+				<div class="margin-bottom">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+				</div>
+		   </div>
+		</div>
+		<div class="overview-map-container text-center margin-bottom-lg">
+			<img class="overview-map" src="/img/layout/overview-map.png">
+		</div>
+	 </div>
 </div>

1b884df55020ef0c60d478c6dfd1306a337bbeb7
interactis <dominik@interactis.ch>
2021-05-18 10:04:55 +0200

    Overview map

--- frontend/views/site/_intro.php
+++ frontend/views/site/_intro.php
@@ -1,5 +1,5 @@
 <div id="intro">
-	 <div class="container">
+	<div class="container">
 		<div class="row">
 		   <div class="col-md-9 col-lg-7 col-xl-6">
 				<h1 class="margin-bottom-md">
@@ -10,8 +10,9 @@
 				</div>
 			</div>
 		</div>
-		<div class="overview-map-container text-center margin-bottom-lg">
-			<img class="overview-map" src="/img/layout/overview-map.png">
-		</div>
-	 </div>
+	</div>
+	 
+	<div class="overview-map-container text-center margin-bottom-lg">
+		<img class="overview-map" src="/img/layout/map.svg">
+	</div>
 </div>

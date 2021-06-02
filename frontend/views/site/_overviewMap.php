402cd1fb6eb377738b262714e4c31172c911c85b
interactis <dominik@interactis.ch>
2021-06-02 13:30:58 +0200

    Popover

--- frontend/views/site/_overviewMap.php
+++ frontend/views/site/_overviewMap.php
@@ -2,22 +2,19 @@
 use common\models\Heritage;
 ?>
 
-<div class="overview-map-container text-center">
+<div class="overview-map-container text-center container">
 	<div class="overview-map">
-		<div class="map-img-container">
-			<img class="map-img" src="/img/layout/map.svg" alt="<?= Yii::t('app', 'Map') ?>">
-		
-			<?php
-			foreach(Heritage::getActiveHeritages() as $model): ?>	
-				<div class="poi" 
-					style="top:<?= $model->map_position_y ?>%; left: <?= $model->map_position_x ?>%;" 
-					data-toggle="popover" 
-					data-placement="top" 
-					data-html="true" 
-					data-content="<?= $this->render('_popover', ['model' => $model]) ?>">
-				</div>
-			<?php endforeach; ?>
-		</div>
-		
+		<img class="map-img" src="/img/layout/map.svg" alt="<?= Yii::t('app', 'Map') ?>">
+	
+		<?php
+		foreach(Heritage::getActiveHeritages() as $model): ?>	
+			<div class="poi" 
+				style="top:<?= $model->map_position_y ?>%; left: <?= $model->map_position_x ?>%;" 
+				data-toggle="popover" 
+				data-placement="top" 
+				data-html="true" 
+				data-content="<?= $this->render('_popover', ['model' => $model]) ?>">
+			</div>
+		<?php endforeach; ?>
 	</div>
 </div>
--- frontend/views/site/_popover.php
+++ frontend/views/site/_popover.php
@@ -5,13 +5,19 @@ $url = Url::toRoute(['heritage/view', 'slug' => $model->slug]);
 $img = $model->previewImage;
 ?>
 
-<a class='img-link' href='<?= $url ?>'>
-	<img class='img-fluid' src='<?= $img['url'] ?>'>
-</a>
-<div class='h4'>
-	<a href='<?= $url ?>'><?= $model->short_name ?></a>
+<div class='card preview-card popover-card'>	
+	<div class='card-header img-header'>
+		<a class='img-link' href='<?= $url ?>'>
+			<img class='img-fluid' src='<?= $img['url'] ?>'>
+		</a>
+	</div>
+	<div class='card-body small'>
+		<div class='h4 margin-bottom-sm'>
+			<a href='<?= $url ?>'><?= $model->short_name ?></a>
+		</div>
+		<a href='<?= $url ?>'>
+			<i class='fa fa-chevron-right'></i> 
+			<?= Yii::t('app', 'Learn more') ?>
+		</a>
+	</div>
 </div>
-<a href='<?= $url ?>'>
-	<i class='fa fa-chevron-right'></i> 
-	<?= Yii::t('app', 'Learn more') ?>
-</a>

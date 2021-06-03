bcc75b129b9d4e02cf94d819a1f39ad8bd6af8f4
interactis <dominik@interactis.ch>
2021-06-03 13:42:59 +0200

    Update page.php

--- frontend/views/site/page.php
+++ frontend/views/site/page.php
@@ -12,7 +12,7 @@ $this->params['breadcrumbs'][] = $this->title;
 	   <div class="section-wrapper">  
 			 <header class="container">
 				<div class="row">
-				   	<div class="col-md-10 col-lg-8 col-xl-7">
+				   	<div class="col-md-10 col-lg-8">
 						<h1 class="margin-bottom-md">
 							<?= $this->title ?>
 						</h1>
@@ -21,7 +21,7 @@ $this->params['breadcrumbs'][] = $this->title;
 			 </header>
 			 <div class="container small">
 			 	<div class="row">
-			 		<div class="col-md-10 col-lg-8 col-xl-7">
+			 		<div class="col-md-10 col-lg-8">
 						<?= $model->description ?>
 					</div>
 				</div>

3c42a51e00cda18c69f4028b0725c9a5e66edcf4
interactis <dominik@interactis.ch>
2021-06-08 11:05:42 +0200

    Update _info.php

--- frontend/views/article/_info.php
+++ frontend/views/article/_info.php
@@ -9,7 +9,7 @@
 					<?= $model->title ?>
 				</h1>
 			  
-			  	<div class="margin-bottom-lg">
+			  	<div class="margin-bottom-lg ugc">
 			  		
 			  		<div class="excerpt margin-bottom-md">
 						<?= $model->excerpt ?>

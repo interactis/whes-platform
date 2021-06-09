c4237a2c2466b5d99fdb9db890557c02f44b10f0
interactis <dominik@interactis.ch>
2021-06-09 18:20:07 +0200

    Update _rucksackButton.php

--- frontend/views/common/_rucksackButton.php
+++ frontend/views/common/_rucksackButton.php
@@ -1,20 +1,16 @@
 <?php
+/*
 $js = "
 	$(document).on('click', '.rucksack-btn', function(e) {
 		e.preventDefault();
 		var btn = $(this);
 		var contentId = btn.attr('content-id');
-		
-		//var counter = $('#bookmark-count');
-		//var count = parseInt(counter.text());
 	
 		if (btn.hasClass('active') === true) {
 			btn.removeClass('active');
-			// count = count-1;
 		}
 		else {
 			btn.addClass('active');
-			// count = count+1;
 		}
 		
 		$.ajax({
@@ -22,25 +18,13 @@ $js = "
 			url: '/rucksack/toggle',
 			data: {id: contentId},
 			success: function(data) {
-				
-				/*
-				var bookmarkCount = $('#bookmark-count');
-		
-				if (count == 0) {
-					bookmarkCount.addClass('hidden');
-				}
-				else {
-					bookmarkCount.removeClass('hidden');
-				}
-		
-				bookmarkCount.text(('0' + count).slice(-2));
-				*/
 			}
 		});
 	});
 ";
 
 $this->registerJs($js, $this::POS_READY);
+*/
 
 $class = "small";
 $showActionText = false;

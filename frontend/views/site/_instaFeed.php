40c490f7b06488f61b15008b606a2300c1894c82
interactis <dominik@interactis.ch>
2021-07-14 14:05:43 +0200

    Update _instaFeed.php

--- frontend/views/site/_instaFeed.php
+++ frontend/views/site/_instaFeed.php
@@ -1,4 +1,4 @@
 <div class="container margin-bottom-lg">
-	<div class="h2 margin-bottom">@WorldHeritageSwitzerland</div>
+	<div class="h2 margin-bottom">@World&shy;Heritage&shy;Switzerland</div>
 	<!-- LightWidget WIDGET --><script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/8e6fddd5850b5b3f840a636d17f5caad.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>
 </div>

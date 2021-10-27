e41482ee6e1be12cf4806d06092c6d4f45c0c1fa
interactis <dominik@interactis.ch>
2021-10-27 17:21:42 +0200

    Update AppAsset.php

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,12 +12,12 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/theme.min.css?v=1.01',
+        'css/theme.min.css?v=1.02',
     ];
     public $js = [
-    	'js/map.js?v=1.01',
+    	'js/map.js?v=1.02',
     	'js/popper.min.js',
-    	'js/theme.js?v=1.01',
+    	'js/theme.js?v=1.02',
     ];
     public $depends = [
         'yii\web\YiiAsset'

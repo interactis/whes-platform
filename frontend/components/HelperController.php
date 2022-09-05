f24063300a97bac96589d9eede3f066addd77dd3
interactis <dominik@interactis.ch>
2022-09-05 16:16:07 +0200

    Filter handling

--- frontend/components/HelperController.php
+++ frontend/components/HelperController.php
@@ -278,4 +278,74 @@ class HelperController extends Controller
 			->where(['in', 'id', $ids])
 			->all();
 	}
+	
+	public function getHeritageFilters($heritage)
+    {
+    	$flagCacheName = $heritage->flagCacheName;
+    	//Yii::$app->cache->delete($flagCacheName); // reset
+    	
+    	if ($flagStr = Yii::$app->cache->get($flagCacheName))
+    	{
+    		$flagIds = explode(',', $flagStr);
+    	}
+    	else
+    	{
+    		$models = $this->_findActiveContent($heritage->id);
+    		$flagIds = $this->_getFlagIds($models);
+    		Yii::$app->cache->set($flagCacheName, implode(',', $flagIds), 3600*4); // 4 hours
+    	}
+    	
+    	return $flagIds;
+    }
+    
+    private function _findActiveContent($heritageId)
+    {	
+    	$query = Content::find()
+    		->leftJoin('article', 'article.content_id = content.id')
+			->leftJoin('poi', 'poi.content_id = content.id')
+			->leftJoin('route', 'route.content_id = content.id')
+			->leftJoin('event', 'event.content_id = content.id')
+			->leftJoin('article_translation', 'article_translation.article_id = article.id')
+			->leftJoin('poi_translation', 'poi_translation.poi_id = poi.id')
+			->leftJoin('route_translation', 'route_translation.route_id = route.id')
+			->leftJoin('event_translation', 'event_translation.event_id = event.id');
+    	
+    	$query->where([
+    		'heritage_id' => $heritageId,
+    		'published' => true,
+    		'hidden' => false,
+    		'approved' => true,
+    		'archive' => false
+    	]);
+    	
+		$query->andFilterWhere(['or',
+			['article_translation.language_id' => \Yii::$app->params['preferredLanguageId']],
+			['poi_translation.language_id' => \Yii::$app->params['preferredLanguageId']],
+			['route_translation.language_id' => \Yii::$app->params['preferredLanguageId']],
+			['event_translation.language_id' => \Yii::$app->params['preferredLanguageId']]
+		]);
+    	
+    	$query->andFilterWhere(['OR', 
+    		['>=', 'event.to', date('Y.m.d')],
+    		['!=', 'content.type', Content::TYPE_EVENT],
+    	]);
+    	
+    	return $query->all();
+    }
+    
+    private function _getFlagIds($models) {
+    	$flagIds = [];
+    	foreach ($models as $model)
+    	{
+    		$contentFlags = $model->contentFlags;
+    		
+    		foreach ($contentFlags as $contentFlag)
+    		{
+				if (!in_array($contentFlag->flag_id, $flagIds))
+					$flagIds[] = $contentFlag->flag_id;
+    		}
+    	}
+    	
+    	return $flagIds;
+    }
 }

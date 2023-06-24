0cc93212bced5ef0b0b310512723334d1cd019ac
interactis <dominik@interactis.ch>
2023-06-24 16:15:23 +0200

    Colors

--- frontend/views/layouts/_svg/edu/heritage.php
+++ frontend/views/layouts/_svg/edu/heritage.php
@@ -1,7 +1,7 @@
 <svg id="heritage-icon" class="icon" xmlns="http://www.w3.org/2000/svg" width="38.061" height="31.718" viewBox="0 0 38.061 31.718">
   <path d="M12.687,29,0,31.718v-29L12.687,0V29" fill="#8FB4D6" fill-rule="evenodd"/>
-  <path d="M13,29V0L26.593,2.719v29Z" transform="translate(-1.219)" fill="#1172C9" fill-rule="evenodd"/>
+  <path d="M13,29V0L26.593,2.719v29Z" transform="translate(-1.219)" fill="#105B9F" fill-rule="evenodd"/>
   <path d="M40.687,29,28,31.718v-29L40.687,0V29" transform="translate(-2.626)" fill="#8FB4D6" fill-rule="evenodd"/>
   <path d="M16,15.364c0,2.411,4.078,8.323,4.078,8.323s4.078-5.913,4.078-8.323A4.229,4.229,0,0,0,20.078,11,4.229,4.229,0,0,0,16,15.364" transform="translate(-1.5 -1.032)" fill="#fff" fill-rule="evenodd"/>
-  <path d="M19.205,18.105a2.177,2.177,0,0,0,2.9-2.9,1.951,1.951,0,0,0-1-1,2.176,2.176,0,0,0-2.9,2.9,1.953,1.953,0,0,0,1,1" transform="translate(-1.688 -1.313)" fill="#1172C9" fill-rule="evenodd"/>
+  <path d="M19.205,18.105a2.177,2.177,0,0,0,2.9-2.9,1.951,1.951,0,0,0-1-1,2.176,2.176,0,0,0-2.9,2.9,1.953,1.953,0,0,0,1,1" transform="translate(-1.688 -1.313)" fill="#105B9F" fill-rule="evenodd"/>
 </svg>
